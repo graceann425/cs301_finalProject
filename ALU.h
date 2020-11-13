f028540ee72a94551451165e987e87ccbac7fbb0
Kent <ksol@DESKTOP-U6PCIOO.localdomain>
2020-11-13 01:37:03 -0500

    Changed input1 and input2 type from int to string

--- ALU.h
+++ ALU.h
@@ -6,31 +6,31 @@
 using namespace std;
 
 class ALU{
-   public: 
+   public:
 	// By default, have constructor set operation to add
 	ALU();
 	~ALU() {};
 
 	// Runs the operation on the two inputs and returns an output
 	string operate(string in1, string in2);
-	
+
 	// Sets the current operation of this ALU. The parameter ALUOp is the output of the ALU Control Unit, so it’s a number in binary representation
 	void setOperation(string ALUOp);
-	
+
 	// All operations the ALU can do. Method operate() will call one of these depending on the current value of ALUOperation.
 	string AND();
 	string OR();
 	string subtract();
 	string add();
 	string setLessThan();
-	
+
 	// print all values associated with this ALU
 	void printALU();
 
-   private: 
+   private:
 	// Need to store for printing purposes
-	int input1;
-	int input2;
+	string input1;
+	string input2;
 	string ALUOperation;
 	string output;
 };
