5d62a02c2e7c4bd04f478a58332b9786d99abe99
graceann425 <43733071+graceann425@users.noreply.github.com>
2020-11-12 21:20:54 -0500

    Add files via upload

---
+++ MainProcessor.h
@@ -0,0 +1,46 @@
+#ifndef _MAINPROCESSOR_H
+#define _MAINPROCESSOR_H
+
+class MainProcessor{
+	class MainProcessor{
+	    public:
+		MainProcessor(string config_filename); 
+		~MainProcessor() {};
+
+		void fetch();
+
+		void decode();
+
+		void execute();
+
+		void memory(string address, string data);
+
+		void writeback(string data);
+
+	    private:
+		// Values obtained from config file
+		string output_mode;   // either to single_step or batch
+		boolean debug_mode;
+		boolean print_memory_contents;
+		boolean write_to_file; 
+		string output_file;  
+
+		string jumpAddress; // The current jump address; if none, set value to 0x400000
+
+		// Classes and objects needed to run processor
+		ASMParser parser;
+		vector<Instruction> instructions; // get from ASMParser
+		Instruction currentInstruction;     // current instruction being processed
+		DataMemory dataMem;
+		RegisterMemory registerMem;
+		RegisterFile registerFile;
+		PC pc;
+		ALU alu1, alu2, alu3;
+		ALUControlUnit ALUControl;
+		controlUnit mainControlUnit;
+		multiplexer mux1, mux2, mux3, mux4, mux5;
+		shiftLeft shiftL1, shiftL2;
+		signExtend signExtend32;
+	};
+
+#endif
