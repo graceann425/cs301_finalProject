90a50c38c0a1684992ba8efa8782a72a0a6b1947
Apsidel <46179649+Apsidel@users.noreply.github.com>
2020-11-13 17:05:37 -0500

    Add files via upload

---
+++ DataMemory.cpp
@@ -0,0 +1,16 @@
+DataMemory(string filename){
+  ifstream input;
+  input.open(filename.c_str());
+
+  if(input.fail()){
+    cout << "Error: Bad format in your file" << filename << endl;
+  }
+
+  else{
+
+  }
+
+
+
+
+}//end setInData() funtion
---
+++ DataMemory.h
@@ -0,0 +1,70 @@
+#ifndef __DATAMEMORY_H__
+#define __DATAMEMORY_H__
+
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class DataMemory{
+  public:
+    //constructor
+    DataMemory(string filename);
+    //deconstructor
+    ~DataMemory() {};
+
+    //data to write
+    void setInData(string data){
+      InData = data
+    };
+
+    //address to write to or read from
+    void setInAddress(string address){
+      InAddress = address
+    };
+
+    //data read from InAddress
+    string getOutData(){
+      return OutData
+    };
+
+    /*finds the data stored in InAddress, to get data after this calculation, use
+      the previous function getOutData()
+    */
+    void readData();
+
+    /* Writes InData to the InAddress.
+        The methods setInData() and setInAddress() must have been called beforehand.
+    */
+    void writeData();
+
+    //print InAddress, InData, and OutData
+    void printInputsAndOutput();
+
+    //print all the memory stored in this object
+    void printDataMemory() {};
+
+  private:
+    string InAddress;
+    string InData;
+    string OutData;
+
+    //stores address and data pairs
+    struct MemoryEntry{
+      string address;
+      string data;
+
+      MemoryEntry(){
+        address = "";
+        data = "";
+      }; //end MemoryEntry()
+
+    }; //end MemoryEntry
+
+    //stores all memory entries from the memory file
+    vector<MemoryEntry>memory;
+};
+
+#endif
