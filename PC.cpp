e7c38b1f97098fe0fce55be3ce27e7f2925f4821
Kent <ksol@DESKTOP-U6PCIOO.localdomain>
2020-12-01 16:33:45 -0500

    Made changes to toString()

--- PC.cpp
+++ PC.cpp
@@ -4,7 +4,7 @@ string PC::toString(){
   stringstream s;
 
   s << "PC \n"
-    << "Current Address: 0x" << myAddress << "\n";
+    << "Next Address: 0x" << myAddress << "\n";
 
   return s.str();
 }
