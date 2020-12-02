48f393c968ee4579072e6d64e25d38ddf45b7beb
graceann425 <43733071+graceann425@users.noreply.github.com>
2020-12-01 21:45:38 -0500

    fixed setLessThan, added comments

--- ALU.cpp
+++ ALU.cpp
@@ -1,5 +1,7 @@
 #include "ALU.h"
 #include <iostream>
+
+//constructor (defaults to add and all inputs as 0)
 ALU::ALU()
 {
 	name = "";
@@ -9,6 +11,7 @@ ALU::ALU()
 	output = "0";
 }
 
+//operate: takes in two input strings and calls the ALU operation
 string ALU::operate(string in1, string in2)
 {
 	input1 = in1;
@@ -25,45 +28,50 @@ string ALU::operate(string in1, string in2)
 		return setLessThan(); //set on less than (0111)
 }
 
+//setOperation: sets ALU operation code
 void ALU::setOperation(string ALUOp)
 {
 	ALUOperation = ALUOp;
 }
 
+//AND: and of two inputs
 string ALU::AND()
 {
-	output = "00000000000000000000000000000000";
-	for (int i = 0; i < static_cast<int>(input1.length()); i++)
+	output = "00000000000000000000000000000000"; //starts output as all zeros
+	for (int i = 0; i < static_cast<int>(input1.length()); i++) 
 	{
-		if (input1[i] == '1' && input2[i] == '1')
-			output[i] = '1';
+		if (input1[i] == '1' && input2[i] == '1') 
+			output[i] = '1'; //changes output to 1 if both inputs are 1
 	}
 	return output;
 }
 
+//OR: or of two inputs 
 string ALU::OR()
 {
-	output = "00000000000000000000000000000000";
+	output = "00000000000000000000000000000000"; //starts output as all zeros
 	for (int i = 0; i < static_cast<int>(input1.length()); i++)
 	{
 		if (input1[i] == '1' || input2[i] == '1')
-			output[i] = '1';
+			output[i] = '1'; //changes output to 1 if either input is 1
 	}
 	return output;
 }
 
+//SUBTRACT: subtracts input 2 from input 1 (adds input 1 and twos complement of input 2)
 string ALU::SUBTRACT()
 {
-	for (int i = 0; i < static_cast<int>(input1.length()); i++)
+	for (int i = 0; i < static_cast<int>(input2.length()); i++)
 	{
-		if (input1[i] == '1')
-			input1[i] = '0';
+		if (input2[i] == '1')
+			input2[i] = '0';
 		else
-			input1[i] = '1';
+			input2[i] = '1';
 	}
 	return ADD();
 }
 
+//ADD: adds input 1 and input 2
 string ALU::ADD()
 {
 	output = "00000000000000000000000000000000";
@@ -87,27 +95,34 @@ string ALU::ADD()
 	return output;
 }
 
+//setLessThan: if input 1 < input 2, output = 1, otherwise output = 0
 string ALU::setLessThan()
 {
-	// if (input1 < input2)
-	// 	output = 1;
-	// else
-	// 	output = 0;
+	output = "00000000000000000000000000000000"; //set default output to 0
+	int less = 0;
+	for (int i = static_cast<int>(input1.length()-1); i >= 0; i--)
+	{
+		if (input1[i] == '1' && input2[i] == '0')
+			less = 1;
+		if (input1[i] == '0' && input2[i] == '1' && less == 0)
+			output = "00000000000000000000000000000001";
+	}
 	return output;
 }
 
-
+//setName
 void ALU::setName(string n)
 {
 	name = n;
 }
 
+//getOutput: returns output
 string ALU::getOutput()
 {
 	return output;
 }
 
-
+//toString
 string ALU::toString()
 {
 	stringstream s;
