2615801219512702d560c7ce8875a53cb3592990
graceann425 <43733071+graceann425@users.noreply.github.com>
2020-12-01 21:53:48 -0500

    added comments

--- ShiftLeft.cpp
+++ ShiftLeft.cpp
@@ -1,5 +1,6 @@
 #include "ShiftLeft.h"
 
+//constructor
 ShiftLeft::ShiftLeft()
 {
 	name = "";
@@ -7,24 +8,24 @@ ShiftLeft::ShiftLeft()
 	output = "";
 }
 
-
+//shiftLeft: shifts binary string left by 2 
+//(removes two most significant bits and adds two zeros in the least significant bits)
 string ShiftLeft::shift(string value)
 {
 	string shifted = "";
-
 	shifted = value.substr(2,string::npos) + "00";
 	output = shifted;
 	return output;
 }
 
-
+//shiftAdd: shifts binary string left by 2 without removing 2 most significant bits
 string ShiftLeft::shiftAdd(string value)
 {
 	output = value + "00";
 	return output;
 }
 
-
+//toString
 string ShiftLeft::toString()
 {
 	stringstream s;
