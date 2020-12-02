0d56ebbb476a959313f18abf3d8bca1085d52278
Kent <ksol@DESKTOP-U6PCIOO.localdomain>
2020-12-01 23:28:07 -0500

    Added comments

--- RegisterFile.cpp
+++ RegisterFile.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include "RegisterFile.h"
 
+
+/**
+ * Default constructor initializes all member variables.
+ *
+ */
 RegisterFile::RegisterFile() {
   rm = nullptr;
   R1 = -1;
@@ -12,23 +17,42 @@ RegisterFile::RegisterFile() {
 }
 
 
+/**
+ * Given some data, write it to the writeRegister.
+ *
+ * @param data data to be written
+ */
 void RegisterFile::writeData(string data) {
   rm->setRegisterData(writeRegister, data);
 }
 
 
+/**
+ * Return the data stored in register 1 (R1).
+ *
+ * @return the data in R1
+ */
 string RegisterFile::getDataR1() {
   dataR1 = rm->getRegister(R1);
   return dataR1;
 }
 
 
+/**
+ * Return the data stored in register 2 (R2).
+ *
+ * @return the data in R2
+ */
 string RegisterFile::getDataR2() {
   dataR2 = rm->getRegister(R2);
   return dataR2;
 }
 
 
+/**
+ * Return a string outputing all relevant information stored in this object.
+ *
+ */
 string RegisterFile::toString() {
   stringstream s ;
 
@@ -50,6 +74,10 @@ string RegisterFile::toString() {
 }
 
 
+/**
+ * Reset the value of most member variables for next instruction cycle.
+ *
+ */
 void RegisterFile::reset() {
   R1 = -1;
   R2 = -1;
